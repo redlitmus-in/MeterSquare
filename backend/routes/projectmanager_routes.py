99931944364618951cc18e2bf9e67e67eb477f23
Deepika01012001 <144757040+Deepika01012001@users.noreply.github.com>
2025-10-06 10:44:16 +0530

    Routes change in TD,PM,SS

--- backend/routes/projectmanager_routes.py
+++ backend/routes/projectmanager_routes.py
@@ -1,41 +1,50 @@
 from flask import Blueprint
+from controllers.site_supervisor_controller import *
 from controllers.projectmanager_controller import *
 from utils.authentication import *
 
 pm_routes = Blueprint("pm_routes", __name__, url_prefix='/api')
 
-@pm_routes.route('/craete_pm', methods=['POST'])
-@jwt_required
-def create_pm_route():
-    return create_pm()
-
+#Role based listout a boq
 @pm_routes.route('/pm_boq', methods=['GET'])
 @jwt_required
 def get_all_PM_boqs_route():
     return get_all_pm_boqs()
 
-@pm_routes.route('/all_pm', methods=['GET'])
+#Create site supervisor
+@pm_routes.route('/create_sitesupervisor', methods=['POST'])
 @jwt_required
-def get_all_pm_route():
-    return get_all_pm()
+def create_sitesupervisor_route():
+    return create_sitesupervisor()
 
-@pm_routes.route('/get_pm/<int:user_id>', methods=['GET'])
+#All sitesupervisor list
+@pm_routes.route('/all_sitesupervisor', methods=['GET'])
 @jwt_required
-def get_pm_id_route(user_id):
-    return get_pm_id(user_id)
+def get_all_sitesupervisor_route():
+    return get_all_sitesupervisor()
 
-@pm_routes.route('/update_pm/<int:user_id>', methods=['PUT'])
+#Particular site supervisor view
+@pm_routes.route('/get_sitesupervisor/<int:site_supervisor_id>', methods=['GET'])
 @jwt_required
-def update_pm_route(user_id):
-    return update_pm(user_id) 
+def get_sitesupervisor_id_route(site_supervisor_id):
+    return get_sitesupervisor_id(site_supervisor_id)
 
-@pm_routes.route('/delete_pm/<int:user_id>', methods=['DELETE'])
+#Edit site supervisor
+@pm_routes.route('/update_sitesupervisor/<int:site_supervisor_id>', methods=['PUT'])
 @jwt_required
-def delete_pm_route(user_id):
-    return delete_pm(user_id) 
+def update_sitesupervisor_route(site_supervisor_id):
+    return update_sitesupervisor(site_supervisor_id) 
 
-@pm_routes.route('/assign_projects', methods=['POST'])
+#Delete site supervisor
+@pm_routes.route('/delete_sitesupervisor/<int:site_supervisor_id>', methods=['DELETE'])
 @jwt_required
-def assign_projects_route():
-    return assign_projects() 
+def delete_sitesupervisor_route(site_supervisor_id):
+    return delete_sitesupervisor(site_supervisor_id) 
+
+#Assign site supervisor
+@pm_routes.route('/ss_assign', methods=['POST'])
+@jwt_required
+def assign_projects_sitesupervisor_route():
+    return assign_projects_sitesupervisor()
+
 
--- backend/routes/technical_routes.py
+++ backend/routes/technical_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from controllers.projectmanager_controller import *
 from utils.authentication import jwt_required
 from controllers.techical_director_controller import *
 from controllers.send_boq_client import send_boq_to_client
@@ -20,4 +21,39 @@ def td_mail_send_route():
 @jwt_required
 def send_boq_to_client_route():
     return send_boq_to_client()
+
+@technical_routes.route('/craete_pm', methods=['POST'])
+@jwt_required
+def create_pm_route():
+    return create_pm()
+
+#All project manager listout assign and unassign project
+@technical_routes.route('/all_pm', methods=['GET'])
+@jwt_required
+def get_all_pm_route():
+    return get_all_pm()
+
+#Particular Project manager view
+@technical_routes.route('/get_pm/<int:user_id>', methods=['GET'])
+@jwt_required
+def get_pm_id_route(user_id):
+    return get_pm_id(user_id)
+
+#Edit project manager
+@technical_routes.route('/update_pm/<int:user_id>', methods=['PUT'])
+@jwt_required
+def update_pm_route(user_id):
+    return update_pm(user_id) 
+
+#Delete Project manager
+@technical_routes.route('/delete_pm/<int:user_id>', methods=['DELETE'])
+@jwt_required
+def delete_pm_route(user_id):
+    return delete_pm(user_id) 
+
+#Assign project manager
+@technical_routes.route('/assign_projects', methods=['POST'])
+@jwt_required
+def assign_projects_route():
+    return assign_projects() 
 
